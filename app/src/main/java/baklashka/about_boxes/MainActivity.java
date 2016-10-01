ea8bc5d953d365f762b936e21ccb7d5d246c6eb8
DarkOverlord2504 <kostia.vierieshchaghin@mail.ru>
2016-10-01 13:13:30 +0300

    Приложение теперь горизонтально, отображается версия, есть не красивые кнопки, цвет фона синий.

--- app/src/main/java/baklashka/about_boxes/MainActivity.java
+++ app/src/main/java/baklashka/about_boxes/MainActivity.java
@@ -1,13 +1,16 @@
 package baklashka.about_boxes;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//поставил горизантольно
+        setContentView(R.layout.main_menu_layout);
     }
 }
