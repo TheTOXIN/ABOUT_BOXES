13bba589631ddd797427fc196bdcf89f920e4333
TheTOXIN <thetoksin@mail.ru>
2016-10-01 22:39:15 +0300

    +Интерфейс главного меню +Новая иконка +Куча багов

--- app/src/main/java/baklashka/about_boxes/MainActivity.java
+++ app/src/main/java/baklashka/about_boxes/MainActivity.java
@@ -1,11 +1,8 @@
 package baklashka.about_boxes;
 
-//import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -14,10 +11,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//поставил горизантольно
         setContentView(R.layout.main_menu_layout);
-
-        //Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/custom_font.ttf");
-        //TextView tv = (TextView) findViewById(R.id.textView);
-        //tv.setTypeface(tf);
-
     }
 }
