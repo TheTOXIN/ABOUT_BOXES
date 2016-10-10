e9321fde4b6b9f892502432aaad177a3b57ca749
TheTOXIN <thetoksin@mail.ru>
2016-10-10 18:57:27 +0300

    НУ ТАКОЕ

--- app/src/main/java/baklashka/about_boxes/MainActivity.java
+++ app/src/main/java/baklashka/about_boxes/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,9 +16,6 @@ public class MainActivity extends Activity {
 
     public Animation anim;
     public ImageView logo;
-    public ImageView play;
-    public ImageView achivments;
-    public ImageView settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +28,55 @@ public class MainActivity extends Activity {
         tv.setTypeface(tf);
 
         logo = (ImageView) findViewById(R.id.logo);
-        play = (ImageView) findViewById(R.id.play);
-        achivments = (ImageView) findViewById(R.id.achivments);
-        settings = (ImageView) findViewById(R.id.settings);
 
         anim = AnimationUtils.loadAnimation(this, R.anim.anim_logo);
         logo.startAnimation(anim);
 
-        anim = AnimationUtils.loadAnimation(this, R.anim.anim_menu);
+        final ImageButton play = (ImageButton) findViewById(R.id.play);
+        final ImageButton achivments = (ImageButton) findViewById(R.id.achivments);
+        final ImageButton settings = (ImageButton) findViewById(R.id.settings);
+        final Animation anim = AnimationUtils.loadAnimation(this, R.anim.anim_menu);
+        //Создаём то, через что можно обращаться к UI (поток экрана)
+        final Handler mainHandler = new Handler()
+        {
+            //То что будет происходить при посылании сюда чего-нить
+            public void handleMessage(android.os.Message msg)
+            {
+                switch (msg.what)//извлекаем из msg i (Я НЕ ЗНАЮ КАК)
+                {
+                    case 0:
+                        play.startAnimation(anim);
+                        play.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        achivments.startAnimation(anim);
+                        achivments.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        settings.startAnimation(anim);
+                        settings.setVisibility(View.VISIBLE);
+                        break;
 
-        for (int i = 1; i <= 3; i++) {
-            if (i == 1)
-                play.startAnimation(anim);
-            else if (i == 2)
-                achivments.startAnimation(anim);
-            else if (i == 3)
-                settings.startAnimation(anim);
+                }
+            }
+        };
 
-        }
+        Thread t = new Thread(new Runnable()//Создаём поток, который по очереди посылает команду хендлеру
+        {
+            public void run() {
+                for (int i = 0; i < 3; i++)
+                {
+                    mainHandler.sendEmptyMessage(i);
+                    try{
+                        Thread.sleep(400);
+                    }catch(InterruptedException e){}
+                }
+            }
+        });
+        t.start();
+
     }
 }
+
 
 
